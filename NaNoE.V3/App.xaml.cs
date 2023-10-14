91625638d7fa6b9d94d9fa8ffd7e67cef27d5698
edg3 <loveland.ernest@gmail.com>
2023-10-14 07:02:45 +0200

    base architecture planned

---
+++ NaNoE.V3/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NaNoE.V3;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ NaNoE.V3/PlatformSpec.cs
@@ -0,0 +1,12 @@
+﻿namespace NaNoE.V3;
+
+public enum PlatformType
+{
+    Mobile,
+    Desktop
+}
+
+public static class PlatformSpec
+{
+    public static PlatformType Type { get; set; }
+}
